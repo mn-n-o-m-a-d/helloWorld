d7e687a5b57d6e826fd164c298b64f54484fca09
Manuel <75840084+mn-n-o-m-a-d@users.noreply.github.com>
2023-09-18 17:04:24 +0200

    Add files via upload

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Manuel. ";
+        Integer alter = 33;
+
+        System.out.println("Hallo Welt! Mein Name ist " + name + "Ich bin " + alter + " Jahre alt.");
+
+    }
+}
